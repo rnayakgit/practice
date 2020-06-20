dc9cc1efa8cd5a784d0536b2b8f01008efde8b5f
RNayakIND <ranjannayak2212gmail.com>
2020-06-20 17:02:07 +0530

    controller.go

--- controller/controller.go
+++ controller/controller.go
@@ -1,3 +1,12 @@
 package controller
 
-import ("fmt")
+import (
+	"fmt"
+
+)
+
+func getEmployee(w http.ResponseWriter, r *http.Request){
+log.Println("*****getEmployee Api called************8");
+json.NewEncoder.Encode("");
+
+}
